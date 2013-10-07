daafe1666955d941e20ea944c639ebada033d52d
yhou8 <yhou2000@gmail.com>
2013-10-07 15:52:56 -0500

    first commit

---
+++ gen/edu/illinois/engr/courses/cs241/honors/tetris/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.illinois.engr.courses.cs241.honors.tetris;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
