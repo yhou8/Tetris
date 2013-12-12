9a71ae109e3e31fa057d85a25e4bf965467d3d71
yhou8 <yhou2000@gmail.com>
2013-12-12 10:40:04 -0600

    removed bin/ and gen/

--- gen/edu/illinois/engr/courses/cs241/honors/tetris/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.illinois.engr.courses.cs241.honors.tetris;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
